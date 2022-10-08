881e87449e2b967a6ce694b7410f09d0494f3bac
khaled saleh <saleh208@hotmail.com>
2022-10-08 13:44:37 +0200

    c

--- Node/src/app.js
+++ Node/src/app.js
@@ -1,11 +1,17 @@
 const http = require('http')
 const os = require ('os')
 
+const listenPort= 8080;
+console.log(`kubia server is starting ...`);
+console.log(`local hostname is : ${os.hostname()} , listening in port ${listenPort}`);
 let handler = function(request,response){
-    console.log(`reciving request from docker ps${request.connection.remoteAddress}`);
-    response.writeHead(200);
-    response.end(`You have hit the ${os.hostname()}`);
+    
+    let clientIP = request.connection.remoteAddress;
+    console.log(`reciving request from ${request.url} ,client ip: ${clientIP}`);
+    response.writeHead(200,{"content-type":"text/html"});
+    response.write(`reciving request from ${request.url}  - client ip is ${clientIP} </br>`);
+    response.end(`You have hit host: ${os.hostname()}`);
 }
 
 var www = http.createServer(handler)
-www.listen(8080)
+www.listen(listenPort)
